883d387c10d3e4ba7538a110a467bf3a6e27e030
dieggsy <dieggsy@protonmail.com>
2019-04-05 02:09:15 -0400

    Add flonum-constants

---
+++ private/flonum/math.private.flonum-constants.scm
@@ -0,0 +1,27 @@
+(module math.private.flonum-constants (-max.0
+                                       -max-subnormal.0
+                                       -min.0
+                                       +min.0
+                                       +max-subnormal.0
+                                       +max.0
+                                       epsilon.0)
+  (import scheme
+          (only chicken.flonum maximum-flonum)
+          (only math.private.flonum-bits
+                ordinal->flonum
+                fpnext
+                fpprev
+                fpulp)
+          (only miscmacros ensure))
+
+
+  (define -max.0 (ensure negative? (fpnext -inf.0)))
+  (define -min.0 (ensure negative? (fpprev 0.0)))
+  (define +min.0 (ensure positive? (fpnext 0.0)))
+  (define +max.0 (ensure positive? (fpprev +inf.0)))
+
+  (define +max-subnormal.0 (ensure positive? (ordinal->flonum #xfffffffffffff)))
+  (define -max-subnormal.0 (- +max-subnormal.0))
+
+  ;; The smallest flonum that can be added to 1.0 to get a result != 1.0
+  (define epsilon.0 (ensure positive? (fpulp 1.0))))
