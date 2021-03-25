663aa079acd2ab4c996ca58be364ed68e515c83c
dieggsy <dieggsy@pm.me>
2021-03-24 22:22:26 -0400

    Add some more basic flonum tests

--- tests/flonum.scm
+++ tests/flonum.scm
@@ -2,6 +2,7 @@
 
   (import math.flonum
           math.base
+          (only srfi-1 count)
           (only miscmacros repeat))
 
   (test-group "fpulp-error"
@@ -142,9 +143,80 @@
     (test 1.0 (fpulp-error +max.0 (fpprev +max.0)))
 
     (repeat 1000
-            (let* ([s (random)]
-                   [e (fp (random-integer -1074 1024))]
-                   [x (* s (fpexpt 2.0 e))])
-              (test 1.0 (fpulp-error x (fpnext x))
-                    ;; (format "x = ~a" x)
-                    )))))
+      (let* ([s (random)]
+             [e (fp (random-integer -1074 1024))]
+             [x (* s (fpexpt 2.0 e))])
+        (test 1.0 (fpulp-error x (fpnext x))
+              ;; (format "x = ~a" x)
+              ))))
+
+  ;; ===================================================================================================
+  ;; fllog2
+
+  ;; Make sure it's exact for exact powers of two
+  ;; for ([x  (in-range -1074.0 1024.0)])
+  (test-group "fplog2"
+    (let loop ([x -1074.0])
+      (unless (= x 1024.0)
+        (let* ((y (fpexp2 x))
+               (x0 (fplog2 y)))
+          (test x0 x)))))
+
+  ;; ===================================================================================================
+  ;; fl2 tests
+
+  (test-group "fp2"
+    ;; for* ([x2  (list -inf.0 -max.0 -1.0 -min.0 -0.0 0.0 +min.0 +1.0 +max.0 +inf.0 +nan.0)]
+    ;;        [x1  (list -inf.0 -max.0 -1.0 -min.0 -0.0 0.0 +min.0 +1.0 +max.0 +inf.0 +nan.0)])
+    (let loop ([nums (list -inf.0 -max.0 -1.0 -min.0 -0.0 0.0 +min.0 +1.0 +max.0 +inf.0 +nan.0)])
+      (unless (null? nums)
+        (let* ((x1 (car nums))
+               (x2 (car nums))
+               (n (count (lambda (b) b)
+                         (map (lambda (f) (f x2 x1))
+                              (list fp2rational? fp2infinite? fp2nan?)))))
+          (test-assert (= n 1))
+          ;; (unless (= n 1) (printf "x2 = ~v  x1 = ~v~n" x2 x1))
+          )
+        (loop (cdr nums))))
+
+    #|
+    Tests to add
+
+    (for*: ([x2  (list -inf.0 -max.0 -1.0 -min.0 -0.0 0.0 +min.0 +1.0 +max.0 +inf.0 +nan.0)]
+    [x1  (list -inf.0 -max.0 -1.0 -min.0 -0.0 0.0 +min.0 +1.0 +max.0 +inf.0 +nan.0)])
+    (define n
+    (count (λ: ([b : Boolean]) b)
+    (map (λ: ([f : (Flonum Flonum -> Boolean)])
+    (f x2 x1))
+    (list fl2rational? fl2infinite? fl2nan?))))
+    (unless (= n 1) (printf "x2 = ~v  x1 = ~v~n" x2 x1)))
+
+    fl2=
+    fl2>
+    fl2<
+    fl2>=
+    fl2<=
+
+    (fl2step x2 x1 n/2) twice = (fl2step x2 x1 n)
+    |#
+
+    (test-assert (let-values ([(y2 y1)  (fp+/error +max.hi +max.lo)])
+                   (fp2= y2 y1 +max.hi +max.lo)))
+
+    (test-assert (let*-values ([(y2 y1)  (fp2next +max.hi +max.lo)])
+                   (fp2infinite? y2 y1))))
+
+  ;; ===================================================================================================
+  ;; FPU testing
+
+  ;; (test '()
+  ;;       (parameterize ([print-fp-test-progress? #f])
+  ;;         (test-floating-point 10000)))
+
+  ;; (test-group "Extra tests"
+  ;;   (import (math flonum tests))
+  ;; ;; TODO: figure out how even to understand the extra tests
+  ;;   )
+
+  )
